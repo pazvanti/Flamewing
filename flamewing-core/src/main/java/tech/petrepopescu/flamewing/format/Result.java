85b1111aa32a67037cda4e0f487211616c8ae550
Petre Popescu <pazvanti2003@gmail.com>
2026-04-08 17:28:34 +0300

    Updated redirect to work with Spring Boot 4

--- flamewing-core/src/main/java/tech/petrepopescu/flamewing/format/Result.java
+++ flamewing-core/src/main/java/tech/petrepopescu/flamewing/format/Result.java
@@ -5,16 +5,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 public class Result extends ResponseEntity<Format> {
     public Result(Format format, HttpStatus status) {
         super(format, HttpStatusCode.valueOf(status.value()));
     }
 
     public Result(HttpHeaders headers, HttpStatus status) {
-        super(headers, HttpStatusCode.valueOf(status.value()));
+        super(toMultiValueMap(headers), HttpStatusCode.valueOf(status.value()));
     }
 
     public Result(Format format, HttpHeaders headers, HttpStatus status) {
-        super(format, headers, HttpStatusCode.valueOf(status.value()));
+        super(format, toMultiValueMap(headers), HttpStatusCode.valueOf(status.value()));
+    }
+
+    private static MultiValueMap<String, String> toMultiValueMap(HttpHeaders headers) {
+        if (headers == null) {
+            return null;
+        }
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        headers.forEach(map::addAll);
+        return map;
     }
 }
